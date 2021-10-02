a3629a322958ad1c304cea75ec3960aa3b865000
Scarounet <rondeau.clement@gmail.com>
2021-10-02 13:15:34 +0200

    + add touch control

--- Scripts/Controls.gd
+++ Scripts/Controls.gd
@@ -3,12 +3,16 @@ extends Node2D
 func _unhandled_input(event):
 	if event.is_echo():
 		return
+
 	if event is InputEventMouseButton and event.is_pressed():
 		if event.button_index == BUTTON_LEFT:
-			spawn(get_global_mouse_position())
+			destroy_at_position(get_global_mouse_position())
+	
+	if event is InputEventScreenTouch and event.is_pressed():
+		destroy_at_position(event.position)
 
 
-func spawn(spawn_global_position):
+func destroy_at_position(spawn_global_position):
 	var space_state = get_world_2d().direct_space_state
 	var results = space_state.intersect_point(spawn_global_position)
 	for result in results:
